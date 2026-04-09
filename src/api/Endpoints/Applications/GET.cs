19c86e2fb889e041b09d55997eecaee0ca190b05
PoolPirate <eliaswolf1609@gmail.com>
2026-04-09 17:41:54 +0200

    chore: udpate default login

---
+++ src/api/Endpoints/Applications/GET.cs
@@ -0,0 +1,26 @@
+using Farsight.Rpc.Api.Auth;
+using Farsight.Rpc.Api.Persistence;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace Farsight.Rpc.Api.Endpoints.Applications;
+
+public sealed class GET(AppDbContext dbContext) : EndpointWithoutRequest<List<GET.ApplicationSummary>>
+{
+    public sealed record ApplicationSummary(Guid Id, string Name);
+
+    public override void Configure()
+    {
+        Get("/api/applications");
+        Policies(AuthPolicies.ADMIN_ONLY);
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var applications = await dbContext.ConsumerApplications
+            .Select(a => new ApplicationSummary(a.Id, a.Name))
+            .ToListAsync(ct);
+
+        await Send.OkAsync(applications, ct);
+    }
+}

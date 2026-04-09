d4bee6d06ae9674525536db8b0b3132f0f322090
PoolPirate <eliaswolf1609@gmail.com>
2026-04-09 23:31:59 +0200

    feat: add endpoint to get RPCs with apikey

---
+++ src/api/Endpoints/Rpcs/ByApiKey/GET.cs
@@ -0,0 +1,59 @@
+using Farsight.Rpc.Api.Persistence;
+using Farsight.Rpc.Api.Persistence.Entities.Rpc;
+using Farsight.Rpc.Types;
+using FastEndpoints;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace Farsight.Rpc.Api.Endpoints.Rpcs.ByApiKey;
+
+public sealed class GET(AppDbContext dbContext) : Endpoint<GET.Request, RpcEndpoint[]>
+{
+    public sealed record Request(
+        [property: FromHeader(ApiKeyHeaders.API_KEY)] string ApiKey
+    );
+
+    public sealed class Validator : AbstractValidator<Request>
+    {
+        public Validator()
+        {
+            RuleFor(x => x.ApiKey)
+                .NotNull()
+                .WithMessage("API key is required.");
+        }
+    }
+
+    public override void Configure()
+    {
+        Get("/api/rpcs");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(Request req, CancellationToken ct)
+    {
+        var key = await dbContext.ConsumerApiKeys
+            .AsNoTracking()
+            .Where(k => k.Key == req.ApiKey)
+            .Select(k => new
+            {
+                k.ApplicationId,
+                k.Environment,
+            })
+            .SingleOrDefaultAsync(ct);
+
+        if(key is null)
+        {
+            ThrowError("API key not found.", 404);
+        }
+
+        var rpcs = await dbContext.Rpcs
+            .AsNoTracking()
+            .Where(rpc => rpc.ApplicationId == key!.ApplicationId && rpc.Environment == key.Environment)
+            .OrderBy(rpc => rpc.Chain)
+            .ThenBy(rpc => EF.Property<string>(rpc, "RpcType"))
+            .ThenBy(rpc => rpc.Id)
+            .ToArrayAsync(ct);
+
+        await Send.OkAsync(rpcs, ct);
+    }
+}

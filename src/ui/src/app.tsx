dc78653ae781c835307328d84649777fc0ab3538
PoolPirate <eliaswolf1609@gmail.com>
2026-04-09 17:18:39 +0200

    feat: login flow

--- src/ui/src/app.tsx
+++ src/ui/src/app.tsx
@@ -1,37 +1,16 @@
-import { QueryClientProvider } from "@tanstack/solid-query";
-import { Route, Router } from "@solidjs/router";
-import { Suspense } from "solid-js";
-import { AppShell } from "./components/AppShell";
-import { queryClient } from "./lib/query";
-import AdminPage from "./routes/admin";
-import ApplicationsPage from "./routes/applications";
-import DashboardPage from "./routes";
-import EditEndpointPage from "./routes/endpoints/edit";
-import EndpointsPage from "./routes/endpoints";
-import NewEndpointPage from "./routes/endpoints/new";
-import LoginPage from "./routes/login";
-import ProvidersAdminPage from "./routes/providers-admin";
-import "./app.css";
+import { Router, Route } from "@solidjs/router";
+import { AuthProvider } from "./lib/auth";
+import RequireAuth from "./components/RequireAuth";
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router
-        root={(props) => (
-          <AppShell>
-            <Suspense>{props.children}</Suspense>
-          </AppShell>
-        )}
-      >
-        <Route path="/" component={DashboardPage} />
-        <Route path="/applications" component={ApplicationsPage} />
-        <Route path="/providers-admin" component={ProvidersAdminPage} />
-        <Route path="/endpoints" component={EndpointsPage} />
-        <Route path="/endpoints/new" component={NewEndpointPage} />
-        <Route path="/endpoints/edit" component={EditEndpointPage} />
-        <Route path="/admin" component={AdminPage} />
+    <AuthProvider>
+      <Router root={(props) => <>{props.children}</>}>
         <Route path="/login" component={LoginPage} />
+        <Route path="/" component={() => <RequireAuth><DashboardPage /></RequireAuth>} />
       </Router>
-    </QueryClientProvider>
+    </AuthProvider>
   );
 }

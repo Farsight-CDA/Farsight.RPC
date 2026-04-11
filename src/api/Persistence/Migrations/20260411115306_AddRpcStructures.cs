fe2d02a2cdd14b2bb7db97da337686cdfded38ea
PoolPirate <eliaswolf1609@gmail.com>
2026-04-11 13:53:50 +0200

    chore: db migration

---
+++ src/api/Persistence/Migrations/20260411115306_AddRpcStructures.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Farsight.Rpc.Api.Persistence.Migrations;
+
+/// <inheritdoc />
+public partial class AddRpcStructures : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+        => migrationBuilder.AddColumn<string[]>(
+            name: "Structures",
+            table: "ConsumerApplications",
+            type: "text[]",
+            nullable: false,
+            defaultValue: Array.Empty<string>()
+        );
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+        => migrationBuilder.DropColumn(
+            name: "Structures",
+            table: "ConsumerApplications"
+        );
+}
